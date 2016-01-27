e3d19c7eaf83205635fc38062cbb404ae27d42e3
Jon Davis <jon@snowulf.com>
2016-01-26 20:17:01 -0800

    pfsense update functional

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Use:
 * meteor --settings settings.json
 
+## Things to remember:
+* snmpwalk -On -v 1 -c public 192.168.1.1 ifInOctets.1
+* snmpwalk -On -v 1 -c public 192.168.1.1 ifOutOctets.1
+ * In * 8 * 100 / time * speed
--- homejs.js
+++ homejs.js
@@ -1,8 +1,7 @@
 Weather = new Mongo.Collection("weather");
+PFSense = new Mongo.Collection("pfsense");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
   Template.weatherbox.helpers({
     conditions: function () {
@@ -15,43 +14,78 @@ if (Meteor.isClient) {
       return Weather.findOne('humidity').value;
     }
   });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.pfsensebox.helpers({
+    in: function () {
+      return PFSense.findOne('in').value;
+    },
+    out: function () {
+      return PFSense.findOne('out').value;
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    updateForecast();
+    Meteor.setInterval(function(){updateForecast();}, 1000*60*20); //Every 20mn
 
-    var ForecastApi = Meteor.npmRequire('forecast');
-    var forecast = new ForecastApi({
-      service: 'forecast.io',
-      key: Meteor.settings.ForecastApiKey,
-      units: 'f', // Only the first letter is parsed
-      cache: true,      // Cache API requests?
-      ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
-        minutes: 27,
-        seconds: 45
-        }
-    });
-    forecast.get(Meteor.settings.ForecastLocation, Meteor.bindEnvironment(function(err, result) {
-    if(err) return console.dir(err);
-      //console.dir(weather);
-      //var a = result.currently.summary;
-      Weather.upsert("conditions",{$set: {value: result.currently.summary}});
-      Weather.upsert("temperature",{$set: {value: Math.round(parseFloat(result.currently.temperature, 10))}});
-      Weather.upsert("humidity",{$set: {value: result.currently.humidity * 100}});
-      //var b = );
-      //var c = result.currently.humidity * 100;
-    }));
-  // code to run on server at startup
-
-
+    getPFSense();
+    Meteor.setInterval(function(){getPFSense();}, 1000*10);
   });
+}
 
+function updateForecast(){
+  console.log("updateForecast()");
+  var ForecastApi = Meteor.npmRequire('forecast');
+  var forecast = new ForecastApi({
+    service: 'forecast.io',
+    key: Meteor.settings.ForecastApiKey,
+    units: 'f', // Only the first letter is parsed
+    cache: true,      // Cache API requests?
+    ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
+      minutes: 19,
+      seconds: 0
+      }
+  });
+  forecast.get(Meteor.settings.ForecastLocation, Meteor.bindEnvironment(function(err, result) {
+  if(err) return console.dir(err);
+    //console.dir(weather);
+    Weather.upsert("conditions",{$set: {value: result.currently.summary}});
+    Weather.upsert("temperature",{$set: {value: Math.round(parseFloat(result.currently.temperature, 10))}});
+    Weather.upsert("humidity",{$set: {value: result.currently.humidity * 100}});
+  }));
+}
 
+function getPFSense(){
+  console.log("getPFSense()");
+  //var inOid = oidToArray(Meteor.settings.snmpInOID);
+  //var outOid = oidToArray(Meteor.settings.snmpOutOID);
+
+  var snmp = Meteor.npmRequire('snmp-native');
+  var session = new snmp.Session({ host: Meteor.settings.snmpIP, port: 161, community: Meteor.settings.snmpCommunity});
+
+  getSNMP(session,Meteor.settings.snmpInOID,"in");
+  getSNMP(session,Meteor.settings.snmpOutOID,"out");
+}
+
+function oidToArray(oid){
+  //no needed currently
+  if( oid.charAt( 0 ) === '.' ){ oid = oid.slice( 1 ); }
+
+  var arr = oid.split(".");
+  arr = arr.map(function (val) { return val; });
+
+  return arr;
+}
+
+function getSNMP(session,oidArray,key){
+  //console.log("getSNMP() " + oidArray);
+  session.get({ oid: oidArray },  Meteor.bindEnvironment(function(error, varbinds) {
+    if (error) {
+        console.log('Fail :(');
+    } else {
+        //var date = new Date();
+        PFSense.upsert(key,{$set: {value: varbinds[0].value}});
+    }
+  }));
 }
